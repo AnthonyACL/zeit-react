e513eabb8cac9d5c670a335bbbbad1ad67d0e6fc
samircornejo <samircornejoatoche22@gmail.com>
2025-10-17 00:49:50 -0500

    style: :art: Correcciones de estilos

--- app/(admin)/Chat/page.tsx
+++ app/(admin)/Chat/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { AppSidebar } from '@/app/(admin)/-componentes/app-sidebar'
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
+import {   Carousel,CarouselContent,CarouselItem,CarouselNext,CarouselPrevious } from '@/components/ui/carousel'
 
 export default function Page() {
 	const users = [
@@ -8,16 +9,4 @@ export default function Page() {
 		{ name: 'Ana Torres', role: 'Supervisor', avatar: '/avatars/ana.jpg' },
 		{ name: 'Luis Pérez', role: 'Operador', avatar: '/avatars/luis.jpg' },
 	]
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<div className="bg-white w-full h-[80px] flex items-center px-8 shadow-sm mb-8">
-					<span className="font-bold" style={{ fontSize: 27 }}>Chat</span>
-				</div>
-				Esta pagina esta en proceso de desarrollo 😉
-
-			</SidebarInset>
-		</SidebarProvider>
-	)
 }
